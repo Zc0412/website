af156485ecb8014320339288db80dde8de827b47
less is more <18666270412@163.com>
2022-10-17 23:35:32 +0800

    feat:完成.md文件解析

--- pages/blog/[[...markdownPath]].js
+++ pages/blog/[[...markdownPath]].js
@@ -1,14 +1,14 @@
-import { unified } from "unified";
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import rehypeSanitize from 'rehype-sanitize'
-import rehypeStringify from 'rehype-stringify'
-import {compile} from "@mdx-js/mdx";
-import remarkToc from "remark-toc";
-import { MDXComponents } from "../../components/MDX/MDXComponents";
+import React, {useMemo} from "react";
+import {MDXComponents} from "../../components/MDX/MDXComponents";
 
 export default function Layout(props) {
-  console.log(props)
+  const {content, toc, meta} = props
+  const parsedContent = useMemo(
+    () => JSON.parse(content, reviveNodeOnClient),
+    [content]
+  );
+  const parsedToc = useMemo(() => JSON.parse(toc, reviveNodeOnClient), [toc]);
+
   return (
     <>
       123
@@ -16,15 +16,46 @@ export default function Layout(props) {
   )
 }
 
+// Deserialize a client React tree from JSON.
+function reviveNodeOnClient(key, val) {
+  if (Array.isArray(val) && val[0] === '$r') {
+    // Assume it's a React element.
+    let type = val[1];
+    let key = val[2];
+    let props = val[3];
+    if (type === 'wrapper') {
+      type = Fragment;
+      props = {children: props.children};
+    }
+    if (MDXComponents[type]) {
+      type = MDXComponents[type];
+    }
+    if (!type) {
+      console.error('Unknown type: ' + type);
+      type = Fragment;
+    }
+    return {
+      $$typeof: Symbol.for('react.element'),
+      type: type,
+      key: key,
+      ref: null,
+      props: props,
+      _owner: null,
+    };
+  } else {
+    return val;
+  }
+}
+
 export async function getStaticProps(context) {
   const fs = require('fs');
-  // const { prepareMDX, } = require('../utils/prepareMDX');
+  const {prepareMDX,} = require('../../utils/prepareMDX');
   const rootDir = `${process.cwd()}/content/`;
   const mdxComponentNames = Object.keys(MDXComponents);
 
   // const mdxComponentNames = Object.keys(MDXComponents);
 
-  // // Read MDX from the file.
+  // Read MDX from the file.
   let path = (context.params.markdownPath || []).join('/') || 'index';
 
   // 读取mdx文件内容
@@ -35,43 +66,97 @@ export async function getStaticProps(context) {
     mdxWithFrontmatter = fs?.readFileSync(rootDir + path + '/index.mdx', 'utf8');
   }
   // 解析mdx内容
-  const matter = require('gray-matter')
-  const { content: mdxWithoutFrontmatter, data: mate } = matter(mdxWithFrontmatter)
-  // console.log(mdxWithoutFrontmatter)
+  const fm = require('gray-matter')
+  const {content: mdxWithoutFrontmatter, data: meta} = fm(mdxWithFrontmatter);
 
   let mdxWithFakeImports = mdxComponentNames
     .map((key) => 'import ' + key + ' from "' + key + '";\n')
     .join('\n');
   mdxWithFakeImports += '\n' + mdxWithoutFrontmatter;
 
+  // Turn the MDX we just read into some JS we can execute.
+  const {remarkPlugins} = require('../../plugins/markdownToHtml');
+  const {compile: compileMdx} = await import('@mdx-js/mdx');
+  const visit = (await import('unist-util-visit')).default;
+  const jsxCode = await compileMdx(mdxWithFakeImports, {
+    remarkPlugins: [...remarkPlugins, (await import('remark-gfm')).default],
+    rehypePlugins: [
+      // Support stuff like ```js App.js {1-5} active by passing it through.
+      function rehypeMetaAsAttributes() {
+        return (tree) => {
+          visit(tree, 'element', (node) => {
+            if (node.tagName === 'code' && node.data && node.data.meta) {
+              node.properties.meta = node.data.meta;
+            }
+          });
+        };
+      },
+    ],
+  });
+  console.log(jsxCode);
+  const {transform} = require('@babel/core');
+  const jsCode = await transform(jsxCode, {
+    plugins: ['@babel/plugin-transform-modules-commonjs'],
+    presets: ['@babel/preset-react'],
+  }).code;
 
-  // const html =await import('remark-html');
+  // Prepare environment for MDX.
+  let fakeExports = {};
+  const fakeRequire = (name) => {
+    if (name === 'react/jsx-runtime') {
+      return require('react/jsx-runtime');
+    } else {
+      // For each fake MDX import, give back the string component name.
+      // It will get serialized later.
+      return name;
+    }
+  };
+  const evalJSCode = new Function('require', 'exports', jsCode);
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // THIS IS A BUILD-TIME EVAL. NEVER DO THIS WITH UNTRUSTED MDX (LIKE FROM CMS)!!!
+  // In this case it's okay because anyone who can edit our MDX can also edit this file.
+  evalJSCode(fakeRequire, fakeExports);
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  const reactTree = fakeExports.default({});
 
-  async function markdownToHtml(markdown) {
-    const file = await unified()
-      .use(remarkParse)
-      .use(remarkRehype)
-      .use(rehypeSanitize)
-      .use(rehypeStringify)
-      .use(remarkToc)
-      .process(markdown)
-    console.log(String(file))
-    return String(file)
+  // Pre-process MDX output and serialize it.
+  let {toc, children} = prepareMDX(reactTree.props.children);
+  if (path === 'index') {
+    toc = [];
   }
 
-  console.log("@@@@@@@",markdownToHtml(mdxWithoutFrontmatter))
-  const compiled = await compile(mdxWithFakeImports, { remarkPlugins: [ markdownToHtml],markdownToHtml })
-  // const compiled = await compile(mdxWithFakeImports, {jsx: true})
-  // console.log("------compiled------", compiled.value)
+  const output = {
+    props: {
+      content: JSON.stringify(children, stringifyNodeOnServer),
+      toc: JSON.stringify(toc, stringifyNodeOnServer),
+      meta,
+    },
+  };
 
+  // Serialize a server React tree node to JSON.
+  function stringifyNodeOnServer(key, val) {
+    if (val != null && val.$$typeof === Symbol.for('react.element')) {
+      // Remove fake MDX props.
+      const {mdxType, originalType, parentName, ...cleanProps} = val.props;
+      return [
+        '$r',
+        typeof val.type === 'string' ? val.type : mdxType,
+        val.key,
+        cleanProps,
+      ];
+    } else {
+      return val;
+    }
+  }
 
-  return { props: { id: 1, mate } }
+  return output;
+
 }
 
 
 export async function getStaticPaths() {
-  const { promisify } = require('util');
-  const { resolve } = require('path');
+  const {promisify} = require('util');
+  const {resolve} = require('path');
   const fs = require('fs');
   const readdir = promisify(fs.readdir);
   const stat = promisify(fs.stat);
--- plugins/markdownToHtml.js
+++ plugins/markdownToHtml.js
@@ -1,30 +1,30 @@
-// import { unified } from 'unified'
-const { unified } =  require('unified')
-// import remarkParse from 'remark-parse'
-const remarkParse = require('remark-parse')
-// import remarkRehype from 'remark-rehype'
-const remarkRehype = require('remark-rehype')
-// import rehypeSanitize from 'rehype-sanitize'
-const rehypeSanitize = require('rehype-sanitize')
-// import rehypeStringify from 'rehype-stringify'
-const rehypeStringify = require('rehype-stringify')
+const remark = require('remark');
+const externalLinks = require('remark-external-links'); // Add _target and rel to external links
+const customHeaders = require('./remark-header-custom-ids'); // Custom header id's for i18n
+const images = require('remark-images'); // Improved image syntax
+const unrwapImages = require('remark-unwrap-images'); // Removes <p> wrapper around images
+const smartyPants = require('./remark-smartypants'); // Cleans up typography
 const html = require('remark-html');
 
-async function markdownToHtml(markdown) {
-  const file = await unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeSanitize)
-    .use(rehypeStringify)
-    .use(html)
-    .process(markdown)
-  return file.toString()
-  // console.log(String(file))
-}
-
 module.exports = {
-  remarkPlugins:[
-
+  remarkPlugins: [
+    externalLinks,
+    customHeaders,
+    images,
+    unrwapImages,
+    smartyPants,
   ],
-  markdownToHtml
+  markdownToHtml,
+};
+
+async function markdownToHtml(markdown) {
+  const result = await remark()
+    .use(externalLinks)
+    .use(customHeaders)
+    .use(images)
+    .use(unrwapImages)
+    .use(smartyPants)
+    .use(html)
+    .process(markdown);
+  return result.toString();
 }
